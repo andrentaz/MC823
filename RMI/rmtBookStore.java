b10a3e73e2473098d94027f360b14b793a7f3e68
André <andre@andre-K43E.(none)>
2013-06-09 00:14:16 -0300

    Mudancas para a parte de RMI

---
+++ RMI/rmtBookStore.java
@@ -0,0 +1,11 @@
+/* Andre Nakagaki Filliettaz --------------------------------------------------
+ * rmtBookStore.java ----------------------------------------------------------
+ * ----------------------------------------------------------------------------
+ * Description: remote interface of the bookstore that will be implemented by -
+ * by the server --------------------------------------------------------------
+ * ------------------------------------------------------------------------- */
+
+package client;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
